f8d585aeda18a51967414ae6d02bd1d73c67bd8e
ATHAR TAJ <athartaj123@gmail.com>
2025-04-08 18:52:28 +0530

    Category Commit

---
+++ category/src/main/java/com/example/category/CategoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.category;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootApplication
+public class CategoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategoryApplication.class, args);
+	}
+}
